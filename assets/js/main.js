b00e651cf25a24d46b25e62f43376af4d6403cc1
jamilalisgandarov <jamil.e@code.edu.az>
2016-06-21 00:22:01 +0400

    Search button and input added, navbar fixed, responsive logo created, search button breakpoint on resized fixed, language selector added with functionalities

--- assets/js/main.js
+++ assets/js/main.js
@@ -80,6 +80,7 @@ jQuery(document).ready(function($) {
     newsOverlay();
     $clickedX = 0;
     $movedX = 0;
+    $marginOut = 0;
     $margin = 0;
     $difference = 0;
     $posTop = 0;
@@ -89,50 +90,92 @@ jQuery(document).ready(function($) {
         $(".sliderMainTrack").on("mousemove", function(event) {
             $movedX = event.pageY;
             $difference = $movedX - $clickedX;
-            // console.log($difference);
+
+            console.log($margin + $difference);
+            if(($margin = -316) && $difference < 0) {
+                $difference = 0;
+            } else if ($margin + $difference < (-316)) {
+                $margin = -316;
+            } else if ($margin + $difference > 0) {
+                $margin = 0;
+            } 
             $(".sliderTrack").css({
                 transform: 'translate3d(0px,' + $difference + 'px,0px)',
 
             });
         });
     });
+
+
+    function carouselInterval() {
+        $marginOut = $marginOut - 116;
+        $('.sliderTrack').css('margin-top', '' + $marginOut + 'px');
+        console.log($marginOut);
+        if ($marginOut < -(4 * 116)) {
+            $marginOut = 0;
+        }
+
+    }
     $(document).mouseup(function(event) {
         $(".sliderMainTrack").unbind('mousemove');
         if ($difference != 0) {
             // console.log($difference);
             $margin = $margin + $difference;
+            $marginOut = $margin;
             $difference = 0;
             $(".sliderTrack").css({
                 'margin-top': '' + $margin + 'px',
                 transform: 'translate3d(0px,0px,0px)'
 
             });
+        } 
+        else if($margin + $difference <= (-316)){
+            $margin=-316;
+        }
+        else if($margin+$difference>=0){
+            $margin=0;
+        }
+        else {
+            $difference = 0;
         }
 
     });
     $('.sliderNews').on('click', function() {
         $('.sliderNewsTriangle').css('visibility', 'hidden');
-        $('.sliderNews').css('background-color','#1d1e20');
+        $('.sliderNews').css('background-color', '#1d1e20');
         $(this).children('.sliderNewsTriangle').css({
             'visibility': 'visible',
-            'border-color':'transparent #242527 transparent transparent '
+            'border-color': 'transparent #242527 transparent transparent '
         });
-        $(this).css('background','#242527');
+        $(this).css('background', '#242527');
 
     });
-    $marginOut=0;
-    function carouselInterval(){
-        $marginOut=$marginOut-116;
-        $('.sliderTrack').css('margin-top',''+$marginOut+'px');
 
 
+
+    // setInterval(function() { carouselInterval() }, 4000);
+    $(".searchButton a").mouseover(function(event) {
+        $('.overlayInput').css('width', '0');
+    });
+
+    $('.searchMain input').focus(function(event) {
+        $('.overlayInput').css('width', '0');
+    });
+    $('.searchMain input').focusout(function(event) {
+        $('.overlayInput').css('width', '100%');
+    });
+    $langOn = 0;
+    $(".langSelector li:first-child img").click(function(event) {
+
+        if ($langOn % 2 == 0) {
+            $('.langSelector li').css('display', 'block');
+        } else {
+
+            $('.langSelector li').css('display', 'none');
+        }
+        $langOn++;
+    });
+    if ($('.sliderNews').margin < 768) {
+        alert("ada");
     }
-   setInterval(function(){carouselInterval()},6000);
-    // setInterval(newsOverlay(), 3000);
-    // $('.sliderMain').slick({
-    //     infinite: true,
-    //     slidesToShow: 3,
-    //     slidesToScroll: 3
-    // });
-    // newsOverlay();
 });
